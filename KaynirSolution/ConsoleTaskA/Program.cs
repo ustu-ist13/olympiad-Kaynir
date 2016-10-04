55c6411b606803588b51b747386f7304dba4f9dd
Kaynir <znovdenis@mail.ru>
2016-10-04 22:19:57 +0300

    TaskB complited

--- KaynirSolution/ConsoleTaskA/Program.cs
+++ KaynirSolution/ConsoleTaskA/Program.cs
@@ -14,7 +14,7 @@ namespace ConsoleTaskA
             int f = Convert.ToInt32(Console.ReadLine());
             if ((12 - f) * 45 < 240) Console.WriteLine("YES");
             else Console.WriteLine("NO");
-            Console.ReadLine();      
+            Console.ReadLine();     
         }
     }
 }
---
+++ KaynirSolution/ConsoleTaskB/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace ConsoleTaskB
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string inp = Console.ReadLine();
+            int row = Convert.ToInt16(Regex.Replace(inp, @"[^\d]+", ""));
+            string seat = Regex.Replace(inp, @"[^\D]+", "");
+            if (row <= 2)
+            {
+                string[] window = { "A", "D"};
+                if (window.Contains(seat)) Console.WriteLine("window");
+                else Console.WriteLine("aisle");
+            }
+            else if(row >= 3 && row <= 20)
+            {
+                string[] window = { "A", "F" };
+                string[] aisle = { "B", "C", "D", "E" };
+                if (window.Contains(seat)) Console.WriteLine("window");
+                else if (aisle.Contains(seat)) Console.WriteLine("aisle");
+                else Console.WriteLine("neither");
+            }
+            else if (row >= 21)
+            {
+                string[] window = { "A", "K" };
+                string[] aisle = { "C", "D", "G", "H" };
+                if (window.Contains(seat)) Console.WriteLine("window");
+                else if (aisle.Contains(seat)) Console.WriteLine("aisle");
+                else Console.WriteLine("neither");
+            }
+            Console.ReadLine();
+        }
+    }
+}
