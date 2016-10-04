220818d8f6601474acec33f6b20591efaee9afd9
Kaynir <znovdenis@mail.ru>
2016-10-04 22:25:13 +0300

    Убрал лишнее

--- KaynirSolution/ConsoleTaskA/Program.cs
+++ KaynirSolution/ConsoleTaskA/Program.cs
@@ -10,7 +10,6 @@ namespace ConsoleTaskA
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Введите количество задач, которое собирается решить Гриша за первый час:");
             int f = Convert.ToInt32(Console.ReadLine());
             if ((12 - f) * 45 < 240) Console.WriteLine("YES");
             else Console.WriteLine("NO");
