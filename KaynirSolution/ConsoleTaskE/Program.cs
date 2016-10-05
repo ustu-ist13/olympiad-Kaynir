ea2a19f5abbc17069fed986b8477e95c50a89a75
Kaynir <znovdenis@mail.ru>
2016-10-05 16:55:20 +0300

    TaskE fcking completed

---
+++ KaynirSolution/ConsoleTaskE/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Linq;
+
+namespace ConsoleTaskE
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] amount = Console.ReadLine().Split(' ').Select(x => int.Parse(x)).ToArray();
+            int[] carts = Console.ReadLine().Split(' ').Select(x => int.Parse(x)).ToArray();
+            int droids = Math.Abs(carts.Where(x => x < amount[1]).Select(x => (x - amount[1])).Sum());
+            int bumbums = carts.Where(x => x > amount[1]).Select(x => (x - amount[1])).Sum();
+            Console.WriteLine($"{bumbums} {droids}");
+        }
+    }
+}
